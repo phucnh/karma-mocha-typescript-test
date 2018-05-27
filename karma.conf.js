52b53bc1d7fbc8feaa71996a1fe457abbaa4e419
Nguyen Hong Phuc (フック） <nguyen_phuc@cyberagent.co.jp>
2018-05-27 23:21:36 +0900

    add PhantomJS to browser targets

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', 'PhantomJS'],
     singleRun: true,
     concurrency: Infinity
   });
